fa52d7379b533fe44f42d4cf6c275995ea1a8995
Ron_Tayler <max.urs@yandex.ru>
2023-08-19 17:39:52 +0300

    Тестируем FreeKassa #3

--- src/Controller/PublicApi/FreeKassa.ts
+++ src/Controller/PublicApi/FreeKassa.ts
@@ -7,7 +7,10 @@ import {inject} from "inversify";
 import {PrismaClient} from "../../../prisma/generated/client";
 import {FreeKassa} from "@/lib/FreeKassa";
 import {Request} from "express";
-import io_ts from "io-ts"
+import {
+    type as io_type,
+    string as io_string
+} from "io-ts"
 
 @controller("/public-api/free-kassa")
 export class Controller_PublicAPI_FreeKassa extends BaseHttpController {
@@ -21,18 +24,18 @@ export class Controller_PublicAPI_FreeKassa extends BaseHttpController {
     @httpPost("/notify")
     getPayUrl(@request() req: Request){
         console.log(req.body)
-        const data = io_ts.type({
-            MERCHANT_ID: io_ts.string, // ID Магазина
-            AMOUNT: io_ts.string, // Сумма платежа
-            intid: io_ts.string, // ID в системе FK
-            MERCHANT_ORDER_ID: io_ts.string, // ID заказа в нашей системе
-            P_EMAIL: io_ts.string, // Email пользователя
-            P_PHONE: io_ts.string, // Телефон пользователя
-            CUR_ID: io_ts.string, // ID Валюты с помощью которой оплатили
-            SIGN: io_ts.string, // Подпись данных
-            payer_account: io_ts.string, // Номер карты/счёта
-            commission: io_ts.string, // Комиссия
-            us_inst_id: io_ts.string // ID Инструктора
+        const data = io_type({
+            MERCHANT_ID: io_string, // ID Магазина
+            AMOUNT: io_string, // Сумма платежа
+            intid: io_string, // ID в системе FK
+            MERCHANT_ORDER_ID: io_string, // ID заказа в нашей системе
+            P_EMAIL: io_string, // Email пользователя
+            P_PHONE: io_string, // Телефон пользователя
+            CUR_ID: io_string, // ID Валюты с помощью которой оплатили
+            SIGN: io_string, // Подпись данных
+            payer_account: io_string, // Номер карты/счёта
+            commission: io_string, // Комиссия
+            us_inst_id: io_string // ID Инструктора
         }).decode(req.body)
         console.log(data);
     }
